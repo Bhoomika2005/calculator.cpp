681c1db22d7a70a59a6c80a3193ba8e9956d24f9
Bhoomika2005 <117982521+Bhoomika2005@users.noreply.github.com>
2022-11-11 16:49:08 +0530

    Add files via upload

---
+++ calculator3.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float num1, num2;
+    char oper;
+    cout << "enter the operator:";
+    cin >> oper;
+    cout << "enter two numbers:";
+    cin >> num1 >> num2;
+
+    switch (oper)
+    {
+    case '+':
+        cout << num1 << "+" << num2 << "=" << (num1 + num2);
+        break;
+    case '-':
+        cout << num1 << "-" << num2 << "=" << (num1 - num2);
+        break;
+    case '*':
+        cout << num1 << "*" << num2 << "=" << (num1 * num2);
+        break;
+    case '/':
+        if (num2 != 0.0)
+            cout << num1 << "/" << num2 << "=" << (num1 / num2);
+        else
+            cout << "cannot say";
+        break;
+
+    default:
+        cout << oper << "invalid operator";
+    }
+    return 0;
+}
